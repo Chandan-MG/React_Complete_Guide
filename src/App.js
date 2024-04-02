cd48b9637f3d4225334f18af35a83cd17f6c4b9d
Chandan-MG <chandanmg.98@gmail.com>
2024-04-02 12:02:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import MovieList from './Components/MovieList';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchMoviesHandler(){
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+    
+    const transformMovies = data.results.map(moviedata =>{
+        return{
+          id: moviedata.episode_id,
+          title: moviedata.title,
+          releaseDate: moviedata.release_date,
+          openingText: moviedata.opening_crawl
+        }
+    })
+    setMovies(transformMovies);
+    setIsLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {isLoading&&<p>Loading.......</p>}
+        {!isLoading && movies.length > 0 && <MovieList movies={movies} />}
+      </section>
+    </React.Fragment>
   );
 }
 
 export default App;
+
