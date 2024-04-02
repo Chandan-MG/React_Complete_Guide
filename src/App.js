38f106e4518835f6b76a19e75c4ff4afe95651b2
Chandan-MG <chandanmg.98@gmail.com>
2024-04-02 12:43:58 +0530

    error proof

--- src/App.js
+++ src/App.js
@@ -6,21 +6,33 @@ import MovieList from './Components/MovieList';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, seterror] = useState(null);
 
   async function fetchMoviesHandler(){
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
-    
-    const transformMovies = data.results.map(moviedata =>{
-        return{
-          id: moviedata.episode_id,
-          title: moviedata.title,
-          releaseDate: moviedata.release_date,
-          openingText: moviedata.opening_crawl
-        }
-    })
-    setMovies(transformMovies);
+    seterror(null);
+    try{
+      const response = await fetch('https://swapi.dev/api/film/');
+      if(!response.ok){
+        throw new Error("Something went wrong ....Retrying")
+      }
+      const data = await response.json();
+
+      
+      
+      const transformMovies = data.results.map(moviedata =>{
+          return{
+            id: moviedata.episode_id,
+            title: moviedata.title,
+            releaseDate: moviedata.release_date,
+            openingText: moviedata.opening_crawl
+          }
+      })
+      setMovies(transformMovies);
+      
+    }catch(error){
+      seterror(error.message);
+    }
     setIsLoading(false);
   }
 
@@ -32,6 +44,7 @@ function App() {
       <section>
         {isLoading&&<p>Loading.......</p>}
         {!isLoading && movies.length > 0 && <MovieList movies={movies} />}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
